84f85a2b85c7471d07407ccd9bd8e13223612fb7
Taras Shchybovyk <tshchybo@gmail.com>
2018-06-23 13:46:57 +0300

    Change tx processing functionality.

--- src/eos_stream.c
+++ src/eos_stream.c
@@ -10,25 +10,214 @@ void initTxContext(txProcessingContext_t *context,
     context->sha256 = sha256;
     context->content = processingContent;
     context->state = TX_CHAIN_ID;
-    context->bytesLeftForState = sizeof(chain_id_t);
     cx_sha256_init(context->sha256);
 }
 
-/**
- * Raw Tx format:
- * 1. chain id. 
- * 2. header.
- * 3. context free actions
- * 4. actions
- * 5. transaction extensions
- * 6. context free data1
-*/
+uint8_t readTxByte(txProcessingContext_t *context) {
+    uint8_t data;
+    if (context->commandLength < 1) {
+        PRINTF("readTxByte Underflow\n");
+        THROW(EXCEPTION);
+    }
+    data = *context->workBuffer;
+    context->workBuffer++;
+    context->commandLength--;
+    return data;
+}
 
-/** 
- * Processing should be optimal. As we are accepting a chunks of data, so buffer size could
- * vary, depending on action data. 
- * Try to cache as little as possible as the RAM capasity isn't so big. 
- */
-parserStatus_e parseTx(txProcessingContext_t *context, uint8_t *in, uint32_t size) {
-    return 0;
+static void hashTxData(txProcessingContext_t *context, uint8_t *buffer, uint32_t length) {
+    cx_hash(&context->sha256->header, 0, buffer, length, NULL, 0);
+}
+
+bool tlvValidate(txProcessingContext_t *context) {
+    uint16_t length;
+    os_memmove(&length, context->tlvBuffer + 1, 2);
+    switch (context->tlvBuffer[0]) {
+    case TX_CHAIN_ID:
+        if (sizeof(chain_id_t) != length) {
+            PRINTF("tlvValidate Invalid chain id size\n");
+            return false;
+        }
+        break;
+    case TX_HEADER:
+        if (sizeof(transaction_header_t) != length) {
+            PRINTF("tlvValidate Invalid transaction header size\n");
+            return false;
+        }
+        break;
+    case TX_CONTEXT_FREE_ACTIONS:
+        if (length != 0) {
+            PRINTF("tlvValidate Context free actions are not supported\n");
+            return false;
+        }
+        break;
+    case TX_ACTIONS:
+        if (length == 0) {
+            PRINTF("tlvValidate empty action is not allowed\n");
+            return false;
+        }
+        break;
+    case TX_TRANSACTION_EXTENSIONS:
+        if (length != 0) {
+            PRINTF("tlvValidate transaction extensions are not supported\n");
+            return false;
+        }
+        break;
+    case TX_CONTEXT_FREE_DATA:
+        if (length != 0) {
+            PRINTF("tlvValidate context free data is not supported\n");
+            return false;
+        }
+    default:
+        PRINTF("Invalid Tag\n");
+        return false;
+    }
+
+    return true;
+}
+
+static void processChainId(txProcessingContext_t *context) {
+    uint16_t length;
+    os_memmove(&length, context->tlvBuffer + 1, 2);
+    if (context->state != TX_CHAIN_ID) {
+        PRINTF("processChainId Invalid Tag\n");
+        THROW(EXCEPTION);
+    }
+
+    if (sizeof(chain_id_t) != length) {
+        PRINTF("processChainId Invalid chain id size\n");
+        THROW(EXCEPTION);
+    }
+
+    if (context->currentFieldPos < context->currentFieldLength) {
+        uint32_t length = 
+            (context->commandLength <
+                     ((context->currentFieldLength - context->currentFieldPos))
+                ? context->commandLength
+                : context->currentFieldLength - context->currentFieldPos);
+        
+        hashTxData(context, context->workBuffer, length);
+        context->workBuffer += length;
+        context->commandLength -= length;
+        context->currentFieldPos += length;
+    }
+
+    if (context->currentFieldPos == context->currentFieldLength) {
+        context->state++;
+        context->processingField = false;
+    }
+}
+
+static void processHeader(txProcessingContext_t *context) {
+    uint16_t length;
+    os_memmove(&length, context->tlvBuffer + 1, 2);
+    if (context->state != TX_CHAIN_ID) {
+        PRINTF("processChainId Invalid Tag\n");
+        THROW(EXCEPTION);
+    }
+
+    if (sizeof(chain_id_t) != length) {
+        PRINTF("processChainId Invalid chain id size\n");
+        THROW(EXCEPTION);
+    }
+
+    if (context->currentFieldPos < context->currentFieldLength) {
+        uint32_t length = 
+            (context->commandLength <
+                     ((context->currentFieldLength - context->currentFieldPos))
+                ? context->commandLength
+                : context->currentFieldLength - context->currentFieldPos);
+        
+        uint8_t * pHeader = (char *)(&context->content->header);
+        os_memmove(pHeader + context->currentFieldLength, context->workBuffer, length);
+        
+        context->workBuffer += length;
+        context->commandLength -= length;
+        context->currentFieldPos += length;
+    }
+
+    if (context->currentFieldPos == context->currentFieldLength) {
+        transaction_header_t *header = &context->content->header;
+        uint8_t tmp[16] = { 0 };
+        uint8_t packedBytes;
+
+        hashTxData(context, (uint8_t *)&header->expiration, sizeof(header->expiration));
+        hashTxData(context, (uint8_t *)&header->ref_block_num, sizeof(header->ref_block_num));
+        hashTxData(context, (uint8_t *)&header->ref_block_prefix, sizeof(header->ref_block_prefix));
+        packedBytes = pack_fc_unsigned_int(header->max_net_usage_words, tmp);
+        hashTxData(context, tmp, packedBytes);
+        hashTxData(context, (uint8_t *)&header->max_cpu_usage_ms, sizeif(header->max_cpu_usage_ms));
+        packedBytes = pack_fc_unsigned_int(header->delay_sec, tmp);
+        hashTxData(context, tmp, packedBytes);
+
+        context->state++;
+        context->processingField = false;
+    }
+}
+
+static parserStatus_e processTxInternal(txProcessingContext_t *context) {
+    for(;;) {
+        if (context->state == TX_DONE) {
+            return STREAM_FINISHED;
+        }
+        if (context->commandLength == 0) {
+            return STREAM_PROCESSING;
+        }
+        if (!context->processingField) {
+            // While we are not processing a field, we should TLV parameters
+            bool canDecode = false;
+            while (context->commandLength != 0) {
+                // Feed the TLV buffer until the length can be decoded
+                context->tlvBuffer[context->tlvBufferPos++] =
+                    readTxByte(context);
+                
+                if (context->tlvBufferPos == sizeof(context->tlvBuffer)) {
+                    canDecode = true;
+                    break;
+                }
+            }
+            if (!canDecode) {
+                return STREAM_PROCESSING;
+            }
+            context->currentFieldPos = 0;
+            context->tlvBufferPos = 0;
+            context->processingField = true;
+        }
+        switch (context->state) {
+        case TX_CHAIN_ID:
+            processChainId(context);
+            break;
+        case TX_HEADER:
+            break;
+        case TX_CONTEXT_FREE_ACTIONS:
+            break;
+        case TX_ACTIONS:
+            break;
+        case TX_TRANSACTION_EXTENSIONS:
+            break;
+        case TX_CONTEXT_FREE_DATA:
+            break;
+        default:
+            PRINTF("Invalid RLP decoder context\n");
+            return STREAM_FAULT;
+        }
+    }
+}
+
+parserStatus_e parseTx(txProcessingContext_t *context, uint8_t *buffer, uint32_t length) {
+    parserStatus_e result;
+    BEGIN_TRY {
+        TRY {
+            context->workBuffer = buffer;
+            context->commandLength = length;
+            result = processTxInternal(context);
+        }
+        CATCH_OTHER(e) {
+            result = STREAM_FAULT;
+        }
+        FINALLY {
+        }
+    }
+    END_TRY;
+    return result;
 }
--- src/eos_stream.h
+++ src/eos_stream.h
@@ -10,6 +10,7 @@ typedef struct txProcessingContent_t {
     char accountName[14];
     char actionName[14];
     char actionData[512];
+    transaction_header_t header;
 } txProcessingContent_t;
 
 typedef enum txProcessingState_e {
@@ -25,14 +26,15 @@ typedef enum txProcessingState_e {
 
 typedef struct txProcessingContext_t {
     txProcessingState_e state;
-    uint32_t bytesLeftForState;
-    uint32_t bufferIndex;
     cx_sha256_t *sha256;
+    uint32_t currentFieldLength;
+    uint32_t currentFieldPos;
+    bool processingField;
+    uint8_t tlvBuffer[3];
+    uint32_t tlvBufferPos;
+    uint8_t *workBuffer;
+    uint32_t commandLength;
     txProcessingContent_t *content;
-    transaction_header_t header;
-    bool cfaSizeRead;
-    bool actionSizeRead;
-    bool teSizeRead;
 } txProcessingContext_t;
 
 typedef enum parserStatus_e {
@@ -42,6 +44,6 @@ typedef enum parserStatus_e {
 } parserStatus_e;
 
 void initTxContext(txProcessingContext_t *context, cx_sha256_t *sha256, txProcessingContent_t *processingContent);
-parserStatus_e parseTx(txProcessingContext_t *context, uint8_t *in, uint32_t size);
+parserStatus_e parseTx(txProcessingContext_t *context, uint8_t *buffer, uint32_t length);
 
 #endif // __EOS_STREAM_H__

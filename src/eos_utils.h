2592c1e318e13e7f93b6e723f24de75afbb85d21
Taras Shchybovyk <tshchybo@gmail.com>
2018-07-07 18:51:05 +0300

    Moved out functions to helper files.

--- src/eos_utils.h
+++ src/eos_utils.h
@@ -14,4 +14,14 @@ char* i64toa(int64_t i, char b[]);
 
 bool tlvTryDecode(uint8_t *buffer, uint32_t bufferLength, uint32_t *fieldLenght, bool *valid);
 
+unsigned char check_canonical(uint8_t *rs);
+
+int ecdsa_der_to_sig(const uint8_t *der, uint8_t *sig);
+
+void rng_rfc6979(unsigned char *rnd,
+                 unsigned char *h1,
+                 unsigned char *x, unsigned int x_len,
+                 unsigned char *q, unsigned int q_len,
+                 unsigned char *V, unsigned char *K);
+
 #endif

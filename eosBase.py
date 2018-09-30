b34770f6bb5b584fc1838b0260cf1b2e0f6780c5
Taras Shchybovyk <tshchybo@gmail.com>
2018-09-30 20:32:47 +0300

    Fix encoding parameter for transaction.

--- eosBase.py
+++ eosBase.py
@@ -194,7 +194,7 @@ class Transaction:
 
     @staticmethod
     def parse_auth(data):
-        parameters = struct.pack('L', data['threshold'])
+        parameters = struct.pack('I', data['threshold'])
         key_number = len(data['keys'])
         parameters += struct.pack('B', key_number)
         for key in data['keys']:
@@ -206,12 +206,10 @@ class Transaction:
     
     @staticmethod
     def parse_update_auth(data):
-        parameters = binascii.unhexlify(
-            '80a763cc1c7aa26300000000a8ed32320000000080ab26a701000000010003b6d4fb38dba56d59623c5e2be38b0cdf63f7958cd61d27b1044271bb04cb63c701000000')
-        # parameters = Transaction.name_to_number(data['account'])
-        # parameters += Transaction.name_to_number(data['permission'])
-        # parameters += Transaction.name_to_number(data['parent'])
-        # parameters += Transaction.parse_auth(data['auth'])
+        parameters = Transaction.name_to_number(data['account'])
+        parameters += Transaction.name_to_number(data['permission'])
+        parameters += Transaction.name_to_number(data['parent'])
+        parameters += Transaction.parse_auth(data['auth'])
         return parameters
 
     @staticmethod
